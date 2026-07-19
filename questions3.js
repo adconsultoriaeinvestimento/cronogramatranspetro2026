6b220bbcca237ef1538e5660aa6a915d7f5a774b
adconsultoriaeinvestimento <adconsultoria100@gmail.com>
2026-07-19 02:12:33 -0300

    Add multiple math questions with alternatives

---
+++ questions3.js
@@ -0,0 +1,403 @@
+{
+id:51,
+disciplina:"Matemática",
+assunto:"Conjuntos numéricos",
+
+enunciado:
+"O conjunto dos números naturais é representado por:",
+
+alternativas:[
+
+"N",
+
+"Z",
+
+"Q",
+
+"R",
+
+"I"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:52,
+disciplina:"Matemática",
+assunto:"Operações",
+
+enunciado:
+"Quanto é 25 + 37?",
+
+alternativas:[
+
+"62",
+
+"52",
+
+"72",
+
+"60",
+
+"55"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:53,
+disciplina:"Matemática",
+assunto:"Porcentagem",
+
+enunciado:
+"20% de 200 corresponde a:",
+
+alternativas:[
+
+"40",
+
+"20",
+
+"50",
+
+"60",
+
+"80"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:54,
+disciplina:"Matemática",
+assunto:"Regra de três",
+
+enunciado:
+"Se 2 máquinas produzem 100 peças, 4 máquinas produzirão:",
+
+alternativas:[
+
+"200 peças",
+
+"150 peças",
+
+"300 peças",
+
+"50 peças",
+
+"400 peças"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:55,
+disciplina:"Matemática",
+assunto:"Razão",
+
+enunciado:
+"A razão entre 10 e 5 é:",
+
+alternativas:[
+
+"2",
+
+"5",
+
+"10",
+
+"15",
+
+"0,5"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:56,
+disciplina:"Matemática",
+assunto:"Equação",
+
+enunciado:
+"Resolva: x + 5 = 12",
+
+alternativas:[
+
+"7",
+
+"5",
+
+"12",
+
+"17",
+
+"6"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:57,
+disciplina:"Matemática",
+assunto:"Equação",
+
+enunciado:
+"Resolva: 2x = 20",
+
+alternativas:[
+
+"10",
+
+"5",
+
+"20",
+
+"40",
+
+"2"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:58,
+disciplina:"Matemática",
+assunto:"Juros simples",
+
+enunciado:
+"O juro simples é calculado pela fórmula:",
+
+alternativas:[
+
+"J=C.i.t",
+
+"J=C+i",
+
+"J=C/t",
+
+"J=i/t",
+
+"J=C-i"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:59,
+disciplina:"Matemática",
+assunto:"Média",
+
+enunciado:
+"A média aritmética é obtida por:",
+
+alternativas:[
+
+"Soma dos valores dividida pela quantidade",
+
+"Maior valor",
+
+"Menor valor",
+
+"Multiplicação",
+
+"Subtração"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:60,
+disciplina:"Matemática",
+assunto:"Probabilidade",
+
+enunciado:
+"A probabilidade varia entre:",
+
+alternativas:[
+
+"0 e 1",
+
+"1 e 10",
+
+"10 e 100",
+
+"-10 e 10",
+
+"Somente números inteiros"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:61,
+disciplina:"Matemática",
+assunto:"Geometria",
+
+enunciado:
+"A área do quadrado é calculada por:",
+
+alternativas:[
+
+"lado²",
+
+"2.lado",
+
+"lado+lado",
+
+"base.altura/2",
+
+"π.r²"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:62,
+disciplina:"Matemática",
+assunto:"Geometria",
+
+enunciado:
+"A área do círculo é:",
+
+alternativas:[
+
+"πr²",
+
+"2πr",
+
+"r+r",
+
+"b.h",
+
+"a²+b²"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:63,
+disciplina:"Matemática",
+assunto:"Potenciação",
+
+enunciado:
+"2³ é igual a:",
+
+alternativas:[
+
+"8",
+
+"6",
+
+"9",
+
+"4",
+
+"12"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:64,
+disciplina:"Matemática",
+assunto:"Logaritmos",
+
+enunciado:
+"O logaritmo representa:",
+
+alternativas:[
+
+"O expoente necessário para obter um número",
+
+"Uma soma",
+
+"Uma divisão",
+
+"Uma raiz somente",
+
+"Uma porcentagem"
+
+],
+
+correta:0
+
+},
+
+
+{
+id:65,
+disciplina:"Matemática",
+assunto:"Estatística",
+
+enunciado:
+"A moda representa:",
+
+alternativas:[
+
+"O valor que mais aparece",
+
+"O valor central",
+
+"A média",
+
+"A diferença",
+
+"O maior valor"
+
+],
+
+correta:0
+
+},
